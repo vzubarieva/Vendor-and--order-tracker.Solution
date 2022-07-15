b827d3c44ee9829caec1abf8c5837590fc440bbb
Viktoria <101473740+vzubarieva@users.noreply.github.com>
2022-07-15 10:51:05 -0700

    Initial commit

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using //ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace //ToDoList.Tests
+{
+  [TestClass]
+  public class //ItemTests : IDisposable
+  {
+
+    //public void Dispose()
+    //{
+    //  Item.ClearAll();
+    //}
+
+    //[TestMethod]
+    //public void ItemConstructor_CreatesInstanceOfItem_Item()
+    //{
+    //   Item newItem = new Item("test"); //We pass in "test" as an argument here.
+    //  Assert.AreEqual(typeof(Item), newItem.GetType());
+    //}
+//
+    //[TestMethod]
+    //public void GetDescription_ReturnsDescription_String()
+    //{
+    //  //Arrange
+    //  string description = "Walk the dog.";
+    //  Item newItem = new Item(description);
+//
+    //  //Act
+    //  string result = newItem.Description;
+//
+    //  //Assert
+    //  Assert.AreEqual(description, result);
+    //}
+
+    
+
+
+
+  }
+}
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+namespace //ToDoList.Models
+{
+  public class  // Item
+  {
+    
+  }
+}
