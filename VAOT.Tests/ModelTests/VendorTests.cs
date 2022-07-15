2c5b98dc6d537408eb75b23d724ff69cc15b9580
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-15 14:30:02 -0700

    add fist test  CreatesInstanceOfVendor which is passed

--- VAOT.Tests/ModelTests/VendorTests.cs
+++ VAOT.Tests/ModelTests/VendorTests.cs
@@ -5,39 +5,38 @@ using System;
 
 namespace VAOT.Tests
 {
-  [TestClass]
-  public class //VendorTests : IDisposable
-  {
+    [TestClass]
+    public class VendorTests //: IDisposable
+    {
+        //public void Dispose()
+        //{
+        //  Item.ClearAll();
+        //}
 
-    //public void Dispose()
-    //{
-    //  Item.ClearAll();
-    //}
-
-    //[TestMethod]
-    //public void ItemConstructor_CreatesInstanceOfItem_Item()
-    //{
-    //   Item newItem = new Item("test"); //We pass in "test" as an argument here.
-    //  Assert.AreEqual(typeof(Item), newItem.GetType());
-    //}
-//
-    //[TestMethod]
-    //public void GetDescription_ReturnsDescription_String()
-    //{
-    //  //Arrange
-    //  string description = "Walk the dog.";
-    //  Item newItem = new Item(description);
-//
-    //  //Act
-    //  string result = newItem.Description;
-//
-    //  //Assert
-    //  Assert.AreEqual(description, result);
-    //}
-
-    
+        [TestMethod]
+        public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+        {
+            Vendor newVendor = new Vendor(); //We pass in "test" as an argument here.
+            Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+        //
+        //[TestMethod]
+        //public void GetDescription_ReturnsDescription_String()
+        //{
+        //  //Arrange
+        //  string description = "Walk the dog.";
+        //  Item newItem = new Item(description);
+        //
+        //  //Act
+        //  string result = newItem.Description;
+        //
+        //  //Assert
+        //  Assert.AreEqual(description, result);
+        //}
 
 
 
-  }
+
+
+    }
 }
