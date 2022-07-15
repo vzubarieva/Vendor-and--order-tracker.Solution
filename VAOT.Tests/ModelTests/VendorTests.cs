2adc8050fe8b20f0a0280a8746bf00ae7adb491d
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-15 15:14:37 -0700

    add succesfuly passed 4th test,  GetVendorDescription

--- VAOT.Tests/ModelTests/VendorTests.cs
+++ VAOT.Tests/ModelTests/VendorTests.cs
@@ -8,15 +8,15 @@ namespace VAOT.Tests
     [TestClass]
     public class VendorTests //: IDisposable
     {
-        //public void Dispose()
-        //{
-        //  Item.ClearAll();
-        //}
+        // public void Dispose()
+        // {
+        // Vendor.ClearAll();
+        // }
 
         [TestMethod]
         public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
         {
-            Vendor newVendor = new Vendor("Vendor's name"); //We pass in "test" as an argument here.
+            Vendor newVendor = new Vendor("Vendor's name", "Vendor's description"); //We pass in "test" as an argument here.
             Assert.AreEqual(typeof(Vendor), newVendor.GetType());
         }
 
@@ -24,7 +24,7 @@ namespace VAOT.Tests
         public void GetName_ReturnsName_String()
         {
             string name = "Vendor's Name";
-            Vendor newVendor = new Vendor(name);
+            Vendor newVendor = new Vendor("Vendor's Name", "Vendor's description");
             string result = newVendor.Name;
             Assert.AreEqual(name, result);
         }
@@ -33,11 +33,21 @@ namespace VAOT.Tests
         public void SetName_SetName_String()
         {
             string name = "Vendor's name";
-            Vendor newVendor = new Vendor(name);
+            Vendor newVendor = new Vendor("Vendor's Name", "Vendor's description");
             string updatedName = "Vendor's name2";
             newVendor.Name = updatedName;
             string result = newVendor.Name;
             Assert.AreEqual(updatedName, result);
+        }
+
+        [TestMethod]
+        public void GetVendorDescription_ReturnsVendorDescription_String()
+        {
+            string vendorDescription = "Vendor's description";
+            string name = "Vendor's name";
+            Vendor newVendor = new Vendor(name, vendorDescription);
+            string result = newVendor.VendorDescription;
+            Assert.AreEqual(vendorDescription, result);
         }
 
 
