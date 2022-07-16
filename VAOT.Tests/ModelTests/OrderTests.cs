55e8a0605e334dd47b7037d1dcde954155a46bc0
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-16 15:54:22 -0700

    add 10th test CreatesInstanceOfOrder

---
+++ VAOT.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VAOT.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VAOT.Tests
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        public void OrderConstructor_CreatesInstanceOfOrder_Order()
+        {
+            Order newOrder = new Order();
+            Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+    }
+}
