3df27b445e1faac3e34ba7254a2e8588d8ff9440
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-15 22:39:29 -0700

    add form for submiting new vendor

--- VAOT/Controllers.cs/HomeController.cs
+++ VAOT/Controllers.cs/HomeController.cs
@@ -1,13 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using VAOT.Models;
 
-namespace ToDoList.Controllers
+namespace VAOT.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet("/")]
+        [Route("/")]
         public ActionResult Index()
         {
+            Vendor starterVendor = new Vendor(
+                "Add new vendor to Vendor List",
+                "Vendor's description"
+            );
+            return View(starterVendor);
+        }
+
+        [Route("/vendors/new")]
+        public ActionResult CreateForm()
+        {
             return View();
+        }
+
+        [Route("/vendors")]
+        public ActionResult Create(string name, string vendorDescription)
+        {
+            Vendor myVendor = new Vendor("Vendor's name", "Vendor's description");
+            return View("Index", myVendor);
         }
 
 
--- VAOT/Models/Vendor.cs
+++ VAOT/Models/Vendor.cs
@@ -1,7 +1,29 @@
 using System.Collections.Generic;
 using System;
 
-namespace VOAT.Models
+namespace VAOT.Models
 {
-    public class Vendor { }
+    public class Vendor
+    {
+        public string Name { get; set; }
+        public string VendorDescription { get; set; }
+        private static List<Vendor> _instances = new List<Vendor> { };
+
+        public Vendor(string name, string vendorDescription)
+        {
+            Name = name;
+            VendorDescription = vendorDescription;
+            _instances.Add(this);
+        }
+
+        public static List<Vendor> GetAll()
+        {
+            return _instances;
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+    }
 }
