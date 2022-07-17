dcb6551c16eb722317766fdfdd9270976b30a187
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-16 23:09:10 -0700

    add tests for Get/Set Title and Get/Set OrderDescription

--- VAOT.Tests/ModelTests/OrderTests.cs
+++ VAOT.Tests/ModelTests/OrderTests.cs
@@ -6,13 +6,54 @@ using System;
 namespace VAOT.Tests
 {
     [TestClass]
-    public class OrderTests
+    public class OrderTests //: IDisposable
     {
+        // public void Dispose()
+        //{
+        //    Order.ClearAll();
+        // }
+
         [TestMethod]
         public void OrderConstructor_CreatesInstanceOfOrder_Order()
         {
-            Order newOrder = new Order();
+            Order newOrder = new Order("Order's title", "Order's description", 28.00, "2022-10-26");
             Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+
+        [TestMethod]
+        public void GetTitle_ReturnsTitle_String()
+        {
+            Order newOrder = new Order("Order's title", "Order's description", 28.00, "2022-10-26");
+            string result = newOrder.Title;
+            Assert.AreEqual("Order's title", result);
+        }
+
+        [TestMethod]
+        public void SetTitle_SetTitle_String()
+        {
+            Order newOrder = new Order("Order's title", "Order's description", 28.00, "2022-10-26");
+            string updatedTitle = "Order's Title updated";
+            newOrder.Title = updatedTitle;
+            string result = newOrder.Title;
+            Assert.AreEqual(updatedTitle, result);
+        }
+
+        [TestMethod]
+        public void GetOrderDescription_ReturnsOrderDescription_String()
+        {
+            Order newOrder = new Order("Order's title", "Order's description", 28.00, "2022-10-26");
+            string result = newOrder.OrderDescription;
+            Assert.AreEqual("Order's description", result);
+        }
+
+        [TestMethod]
+        public void SetOrderDescription_SetOrderDescription_String()
+        {
+            Order newOrder = new Order("Order's title", "Order's description", 28.00, "2022-10-26");
+            string updatedOrderDescription = "Order's description updated";
+            newOrder.OrderDescription = updatedOrderDescription;
+            string result = newOrder.OrderDescription;
+            Assert.AreEqual(updatedOrderDescription, result);
         }
     }
 }
